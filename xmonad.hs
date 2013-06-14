c5200de039219969599131a1aeb51dd569abb897
Gerald Ke <gerald@freelancer.com>
2013-06-14 18:18:41 +0800

    add focused border color

--- xmonad.hs
+++ xmonad.hs
@@ -10,6 +10,7 @@ xmproc <- spawnPipe "/usr/bin/xmobar /home/gerald/.xmonad/.xmobarrc"
 xmonad $ defaultConfig
     { manageHook = manageDocks <+> manageHook defaultConfig
     , layoutHook = avoidStruts $ layoutHook defaultConfig
+    , focusedBorderColor = "#00B2EE"
     } `additionalKeys`
     [ ((mod4Mask .|. shiftMask, xK_z), spawn "gnome-screensaver-command --lock") --mod4mask is the windows key
     , ((0, xK_Print), spawn "gnome-screenshot")
